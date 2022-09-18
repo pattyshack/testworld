5eed61bd642f2f38a19a38c029fa5ab0dd24053a
Patrick Lee <pattyshack101@gmail.com>
2022-09-18 10:54:18 -0700

    simplify logging

--- mods/base/logging.lua
+++ mods/base/logging.lua
@@ -1,4 +1,4 @@
-logging = {
+Logger = Class({
   -- log level
   DEBUG = 1,
   INFO = 2,
@@ -27,141 +27,110 @@ logging = {
     -- tree = 1,
     -- water = 1,
   }
-}
-
-function logging.debug_log_level(module_name)
-  local level = logging.debug_log_level_map[module_name]
-  if level ~= nil then
-    return level
-  end
-
-  return logging.debug_log_level_map["default"]
-end
-
-function logging.should_log_debug(module_name, verbose_level)
-  return logging.debug_log_level(module_name) >= verbose_level
-end
-
-function logging.verbose(module_name, verbose_level, template, ...)
-  if logging.should_log_debug(module_name, verbose_level) then
-    minetest.log(
-      "none",
-      "DEBUG[" .. module_name .. "]: " ..
-        string.format(template, ...))
-  end
-end
-
-function logging.debug(module_name, template, ...)
-  logging.verbose(module_name, 0, template, ...)
-end
-
-function logging.info(module_name, template, ...)
-  minetest.log(
-    "none", -- "info" doesn't work ...
-    "INFO[".. module_name .."]: " .. string.format(template, ...))
-end
-
-function logging.warn(module_name, template, ...)
-  minetest.log(
-    "warning",
-    "[".. module_name .."]: " .. string.format(template, ...))
-end
-
-function logging.err(module_name, template, ...)
-  minetest.log(
-    "error",
-    "[".. module_name .."]: " .. string.format(template, ...))
-end
-
-function logging.log(log_level, module_name, template, ...)
-  if log_level == logging.DEBUG then
-    logging.debug(module_name, template, ...)
-  elseif log_level == logging.INFO then
-    logging.info(module_name, template, ...)
-  elseif log_level == logging.WARN then
-    logging.warn(module_name, template, ...)
-  elseif log_level == logging.ERR then
-    logging.err(module_name, template, ...)
-  else
-    minetest.log(
-      "none", -- "info" doesn't work ...
-      "UNKNOWN(".. log_level .. ")[".. module_name .."]: " ..
-        string.format(template, ...))
-  end
-end
-
-local function pretty_log_value(log_level, module_name, value, indent, prefix)
-  if type(value) == "table" then
-    logging.log(log_level, module_name, "%s%s{", indent, prefix)
-    for k, v in pairs(value) do
-      pretty_log_value(
-        log_level,
-        module_name,
-        v,
-        indent .. "    ",
-        k .. " = ")
-    end
-    logging.log(log_level, module_name, indent .. "},")
-  else
-    logging.log(log_level, module_name, "%s%s%s,", indent, prefix, value)
-  end
-end
-
-function logging.pretty_log_value(log_level, module_name, value)
-  pretty_log_value(log_level, module_name, value, "    ", "")
-end
-
-VerboseDebugLogger = Class()
-
-function VerboseDebugLogger:_init(module_name, verbose_level)
-  self.module_name = module_name
-  self.verbose_level = verbose_level
-end
-
-function VerboseDebugLogger:should_log()
-  return logging.should_log_debug(self.module_name, self.verbose_level)
-end
-
-function VerboseDebugLogger:debug(template, ...)
-  logging.verbose(self.module_name, self.verbose_level, template, ...)
-end
-
-function VerboseDebugLogger:pretty_log_value(value)
-  if self:should_log() then
-    logging.pretty_log_value(logging.DEBUG, self.module_name, value)
-  end
-end
-
-Logger = Class()
+})
 
 function Logger:_init(module_name)
   self.module_name = module_name or minetest.get_current_modname()
 end
 
+function Logger:debug_log_level()
+  local level = self.debug_log_level_map[self.module_name]
+  if level ~= nil then
+    return level
+  end
+
+  return self.debug_log_level_map["default"]
+end
+
+function Logger:should_log_debug(verbose_level)
+  return self:debug_log_level() >= verbose_level
+end
+
+function Logger:verbose(verbose_level, template, ...)
+  if self:should_log_debug(verbose_level) then
+    minetest.log(
+      "none",
+      "DEBUG[" .. self.module_name .. "]: " ..
+        string.format(template, ...))
+  end
+end
+
 function Logger:debug(template, ...)
-  logging.debug(self.module_name, template, ...)
+  self:verbose(0, template, ...)
 end
 
 function Logger:info(template, ...)
-  logging.info(self.module_name, template, ...)
+  minetest.log(
+    "none", -- "info" doesn't work ...
+    "INFO[".. self.module_name .."]: " .. string.format(template, ...))
 end
 
 function Logger:warn(template, ...)
-  logging.warn(self.module_name, template, ...)
+  minetest.log(
+    "warning",
+    "[".. self.module_name .."]: " .. string.format(template, ...))
 end
 
 function Logger:err(template, ...)
-  logging.err(self.module_name, template, ...)
+  minetest.log(
+    "error",
+    "[".. self.module_name .."]: " .. string.format(template, ...))
 end
 
 function Logger:log(log_level, template, ...)
-  logging.log(log_level, self.module_name, template, ...)
+  if log_level == Logger.DEBUG then
+    self:debug(template, ...)
+  elseif log_level == Logger.INFO then
+    self:info(template, ...)
+  elseif log_level == Logger.WARN then
+    self:warn(template, ...)
+  elseif log_level == Logger.ERR then
+    self:err(template, ...)
+  else
+    minetest.log(
+      "none", -- "info" doesn't work ...
+      "UNKNOWN(".. log_level .. ")[".. self.module_name .."]: " ..
+        string.format(template, ...))
+  end
+end
+
+function Logger:_pretty_log_value(log_level, value, indent, prefix)
+  if type(value) == "table" then
+    self:log(log_level, "%s%s{", indent, prefix)
+    for k, v in pairs(value) do
+      self:_pretty_log_value(log_level, v, indent .. "    ", k .. " = ")
+    end
+    self:log(log_level, indent .. "},")
+  else
+    self:log(log_level, "%s%s%s,", indent, prefix, value)
+  end
 end
 
 function Logger:pretty_log_value(log_level, value)
-  logging.pretty_log_value(log_level, self.module_name, value)
+  self:_pretty_log_value(log_level, value, "    ", "")
 end
 
 function Logger:v(verbose_level)
-  return VerboseDebugLogger(self.module_name, verbose_level)
+  return VerboseDebugLogger(self, verbose_level)
+end
+
+VerboseDebugLogger = Class()
+
+function VerboseDebugLogger:_init(logger, verbose_level)
+  self.logger = logger
+  self.verbose_level = verbose_level
+end
+
+function VerboseDebugLogger:should_log()
+  return self.logger:should_log_debug(self.verbose_level)
+end
+
+function VerboseDebugLogger:debug(template, ...)
+  self.logger:verbose(self.verbose_level, template, ...)
+end
+
+function VerboseDebugLogger:pretty_log_value(value)
+  if self:should_log() then
+    self.logger:pretty_log_value(Logger.DEBUG, value)
+  end
 end
